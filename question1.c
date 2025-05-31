aad313a012458ab5c4448eb878a32b346b5afec2
Vibhuti00 <273vibhuti@gmail.com>
2025-05-31 12:25:00 +0530

    Add files via upload

--- question1.c
+++ question1.c
@@ -2,10 +2,17 @@
 int main() 
 {
     int num, first_digit, last_digit, sum;
-    printf("Enter three_digit +ve integer: ");
+
+    printf("Enter three_digit +ve integer:");
     scanf("%d", &num);
+    printf("Enter first_digit:");
+    scanf("%d", &first_digit);
+    printf("Enter last_digit:");
+    scanf("%d", &last_digit);
+
     first_digit = num / 100;
-    sum = firstd_digit + last_digit;
-    printf("\n %d The sum of first & last digit is: ", sum);
+    sum = first_digit + last_digit;
+
+    printf("\n %d The sum of first & last digit is:", sum);
     return 0;
 }
